71f9fec43945107e7b580f2a1216a50eb58b0200
Zion Walker <zionwalker1337@gmail.com>
2025-09-22 12:25:39 -0400

    added create and get pools

--- pkg/truenas/pool.go
+++ pkg/truenas/pool.go
@@ -1,5 +1,13 @@
 package truenas
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"k8s.io/klog/v2"
+)
+
 // TODO:Change when it is eventually changed
 type PoolCreate struct {
 	Name                 string  `json:"name"`
@@ -18,6 +26,31 @@ type PoolCreate struct {
 	Topology struct{} `json:"topology"`
 }
 
-func (c *APIClient) CreatePool(data PoolCreate) error {
-	return nil
+func (c *APIClient) CreatePool(ctx context.Context, opts PoolCreate) error {
+	klog.V(2).Infof("Creating ZFS pool: %s", opts.Name)
+
+	if opts.Name == "" {
+		return fmt.Errorf("pool name is required")
+	}
+
+	req := APIRequest{
+		Method: "pool.create",
+		Params: []any{opts},
+	}
+
+	return c.CallWithTimeout(ctx, req, nil, 5*time.Minute)
+}
+
+func (c *APIClient) GetPools(ctx context.Context, filters []any) (map[string]any, error) {
+	klog.V(4).Info("Getting existing pools")
+
+	var res map[string]any
+
+	req := APIRequest{
+		Method: "pool.query",
+		Params: filters,
+	}
+	err := c.Call(ctx, req, &res)
+
+	return res, err
 }
